d63815519dfbf3b2153a8bba5993a6f16a15da04
BABIFAS <babifasp@gmail.com>
2024-06-07 22:12:50 +0530

    Category model added

---
+++ Inventory Management System/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Inventory_Management_System.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+    }
+}
